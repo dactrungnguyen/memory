7a569b2115ca69417da044b23e0b9474268b6083
Dac-Trung Nguyen <dac-trung.nguyen@external.engie.com>
2020-04-28 19:15:35 +0200

    Mettez à jour l'état local avec « setState »

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
 const SIDE = 6
 const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MS = 750
 
 class App extends Component {
-  cards = this.generateCards()
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: [],
+  }
 
   generateCards() {
     const result = []
@@ -22,21 +28,60 @@ class App extends Component {
     return shuffle(result)
   }
   
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+  
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+    
+    // currentPair.length === 2
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+  
+    // currentPair does not include index
+    return indexMatched ? 'visible' : 'hidden'
+  }
+  
+  handleNewPairClosedBy = (index) => {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[currentPair[0]] === cards[index]
+    this.setState({ currentPair: newPair,guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MS)
+  }
+  
   // Arrow fx for binding to "this"
-  handleCardClick = card => {
-    console.log(card, 'clicked', this)
+  handleCardClick = index => {
+    const { currentPair } = this.state
+    if (currentPair.length === 2) {
+      return
+    }
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index]})
+      return
+    }
+    this.handleNewPairClosedBy(index)
   }
 
   render() {
-    const won = new Date().getSeconds() % 2 === 0
+    const { cards, guesses, matchedCardIndices } = this.state
+    const won = matchedCardIndices.length === cards.length
     return (
       <div className="memory">
-        <GuessCount guesses={0} />
-        {this.cards.map((card, index) => (
+        <GuessCount guesses={guesses} />
+        {cards.map((card, index) => (
           <Card 
             card={card}
-            feedback='visible'
+            feedback={this.getFeedbackForCard(index)}
             key={index}
+            index={index}
             onClick={this.handleCardClick}
           />
         ))}
