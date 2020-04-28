dfc1183965f335c46f65fae6fecdf8bf1f928ada
Dac-Trung Nguyen <dac-trung.nguyen@external.engie.com>
2020-04-28 15:47:09 +0200

    Manipulez les listes de composants

--- src/GuessCount.js
+++ src/GuessCount.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import './GuessCount.css'
 
-const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
+const GuessCount = ({ guesses }) => <div className="guesses">Guess count: {guesses}</div>
 
 export default GuessCount
